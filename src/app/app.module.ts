064d20223ce9acebbc153389743be99b71d289ec
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-04-17 15:09:11 -0700

    imported bootstrap for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+//Angular-specific modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+//Components
 import { AppComponent } from './app.component';
 import { IngredientsListComponent } from './ingredients-list/ingredients-list.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { MonthSelectComponent } from './month-select/month-select.component';
+//CSS-Framework-Related
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { MonthSelectComponent } from './month-select/month-select.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
